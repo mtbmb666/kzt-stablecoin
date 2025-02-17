a33969718b09d539738838254e39126721cca0bd
Hoodjie Kilong <misterfighter1990@gmail.com>
2025-02-17 10:53:05 -0800

    implemented checking the chain if its valid

--- output/main.js
+++ output/main.js
@@ -2,4 +2,5 @@ import Blockchain from './Blockchain.js';
 import Block from './Block.js';
 const KZTtChain = new Blockchain();
 KZTtChain.addBlock(new Block(1, '17-02-2025:23-30', { amount: 21_000_000 }));
-console.log(JSON.stringify(KZTtChain, null, 4));
+// console.log(JSON.stringify(KZTtChain))
+console.log('Is chain valid? ' + KZTtChain.isChainValid());
